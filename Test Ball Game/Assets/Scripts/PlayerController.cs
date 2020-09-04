f9dcf73ce3d57056e2c516c53bb57219c7f1691a
sachins602 <59023792+sachins602@users.noreply.github.com>
2020-09-04 20:40:58 +0545

    Almost Working Copy 2

--- Test Ball Game/Assets/Scripts/PlayerController.cs
+++ Test Ball Game/Assets/Scripts/PlayerController.cs
@@ -46,7 +46,7 @@ public class PlayerController : MonoBehaviour
     {
        
 
-        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        powerUpIndicator.transform.position = transform.position + new Vector3(3.34f, -0.5f, 2.9f);
         if (transform.position.y < -10)
         {
             Destroy(gameObject);
